268d28115d468f4b1df00be59a88ce77d5b9d10e
Harshit Saraswat <41959164+harshit-saraswat@users.noreply.github.com>
2020-09-21 23:14:32 +0530

    Implemented React useState Hook for Counter Increase and Decrease Operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return <div />;
-}
+  // var count = 0;
+  const [count, setCount] = React.useState(0);
 
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
 export default App;
