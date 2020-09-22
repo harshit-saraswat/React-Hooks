6e5fadea719138d100150ed8dc334d33034d0c58
Harshit Saraswat <41959164+harshit-saraswat@users.noreply.github.com>
2020-09-22 22:54:06 +0530

    Completed Challenge 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  console.log(time);
+  var [currTime, setcurrTime] = React.useState(time);
+
+  function getTime() {
+    let time = new Date().toLocaleTimeString();
+    setcurrTime(time);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currTime}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
